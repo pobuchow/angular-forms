2ecd801bb1aa17ff421ce7e0fd4a8c69de01607c
pobuchow <pobu1@hotmail.com>
2020-11-05 22:53:30 +0100

    basic text form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccountFormComponent } from './user/account-form/account-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountFormComponent
   ],
   imports: [
     BrowserModule
